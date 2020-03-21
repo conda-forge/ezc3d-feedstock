ece298281cb0cd1b7b9f139ea50a7bab0fc9c0ba
Matthew R. Becker <beckermr@users.noreply.github.com>
2020-03-21 07:17:48 -0500

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,14 +1,14 @@
 mkdir build
 cd build
 
-cmake ../^
-    -G"Visual Studio 14 2015 Win64"^
-    -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"^
-	-DBUILD_EXAMPLE=OFF^
-	-DBINDER_MATLAB=OFF^
-    -DBINDER_PYTHON3=ON^
+cmake ../ ^
+    -GNinja ^
+    -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+	-DBUILD_EXAMPLE=OFF ^
+	-DBINDER_MATLAB=OFF ^
+    -DBINDER_PYTHON3=ON ^
     -DPython3_EXECUTABLE="%PREFIX%/python.exe"
 
-cmake --build ./^
-    --config Release^
+cmake --build ./ ^
+    --config Release ^
     --target install
