d4efa172f7c2e7678f04b6595e7cfaebad20ac54
Pariterre <benjamin.michaud.phq@gmail.com>
2023-01-12 15:55:59 -0500

    Trying better build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,20 +3,18 @@ mkdir build
 cd build
 
 cmake ../ \
-    -DNUMPY_INCLUDES=$PREFIX/numpy/core/include \
+    -GNinja \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_PREFIX_PATH=$PREFIX \
     -DBUILD_EXAMPLE=OFF \
     -DBINDER_MATLAB=OFF \
+    -D$backend_name_in_build=ON
     -DMatlab_ezc3d_INSTALL_DIR=$PREFIX/MATLAB \
     -DOctave_ezc3d_INSTALL_DIR=$PREFIX/Octave \
-    -DCMAKE_PREFIX_PATH=$PREFIX \
-    -DPython_EXECUTABLE=$PREFIX/bin/python \
-    -DPython3_EXECUTABLE=$PREFIX/bin/python \
-    -DCMAKE_BUILD_TYPE=Release \
-    -D$backend_name_in_build=ON
+    -DPython3_EXECUTABLE=$PREFIX/bin/python
 
-make -j $CPU_COUNT
-make install
+ninja install
 
 # Octave can't be tested from meta.yaml because of the install path not know in that file
 if [ $backend == "octave" ] && [ "$OSTYPE" == "darwin" ]; 
