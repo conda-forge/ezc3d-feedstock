db42112b5ed951e44eb0838c7d3fc01f89097103
Pariterre <benjamin.michaud.phq@gmail.com>
2023-01-16 11:15:59 -0500

    help finding python

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,6 +10,7 @@ cmake ../ \
     -DBUILD_EXAMPLE=OFF \
     -DBINDER_MATLAB=OFF \
     -DBINDER_PYTHON3=ON \
-    -DOctave_ezc3d_INSTALL_DIR=$PREFIX/Octave
+    -DOctave_ezc3d_INSTALL_DIR=$PREFIX/Octave \
+    -DPython3_EXECUTABLE=$PREFIX/bin/python
 
 ninja install
