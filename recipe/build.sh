7cfa5a035e3bb6a57706c6f2f6348f2eeb432da7
Morten Enemark Lund <mel@anybodytech.com>
2024-12-04 12:34:40 +0100

    Reoved octave from build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,14 +10,6 @@ cmake ../ \
     -DBUILD_EXAMPLE=OFF \
     -DBINDER_MATLAB=OFF \
     -D$backend_name_in_build=ON \
-    -DOctave_ezc3d_INSTALL_DIR=$PREFIX/Octave \
     -DPython3_EXECUTABLE=$PREFIX/bin/python
 
 ninja install
-
-# Octave can't be tested from meta.yaml because of the install path not known in that file
-# So we test it here
-if [ $backend == "octave" ] && [ "$OSTYPE" == "darwin" ]; 
-then 
-    octave --eval "addpath('$PREFIX/Octave/ezc3d_octave'); c = ezc3dRead();"
-fi
